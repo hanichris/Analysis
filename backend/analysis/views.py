6facbe5e249f9e97a25bf263c320105311b53c36
hanichris <hanichris71@gmail.com>
2024-05-27 18:26:39 +0300

    view to serve react application.

--- backend/analysis/views.py
+++ backend/analysis/views.py
@@ -1,6 +1,13 @@
-from django.http import Http404, HttpRequest
+import os
+import json
+
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request: HttpRequest):
-    raise Http404("What you are looking for is not here.")
+    cur_path = os.path.dirname(__file__)
+    new_path = os.path.relpath('..\\static\\manifest.json', cur_path)
+    manifest = json.load(open(new_path))
+    print(manifest)
+    return render(request, "analysis/dashboard.html", )
