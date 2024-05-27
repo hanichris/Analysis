092af27c8e0aac6dc00341fddb7c86ed3d27e098
hanichris <hanichris71@gmail.com>
2024-05-27 18:23:23 +0300

    vite configuration for javascript tooling to support frontend development in Django.

---
+++ backend/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import * as path from "path";
+
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/static/",
+  build: {
+    manifest: "mainfest.json",
+    outDir: path.resolve('./static'),
+    rollupOptions: {
+      input: {
+        key0001: "./assets/main.tsx"
+      },
+      output: {
+        entryFileNames: "index-bundle.js"
+      }
+    },
+  },
+})
+
