245125a182d19b091b434c3a6fc0f75ec527e8cf
hanichris <hanichris71@gmail.com>
2024-05-27 19:18:20 +0300

    fix: typo

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   base: "/static/",
   build: {
-    manifest: "mainfest.json",
+    manifest: "manifest.json",
     outDir: path.resolve('./static'),
     rollupOptions: {
       input: {
