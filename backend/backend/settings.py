baccc55520aa103d01915a8b49ca5c3130bcad4b
hanichris <hanichris71@gmail.com>
2024-05-28 21:33:50 +0300

    add redirection urls for login and logout functionality.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,3 +129,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Login redirect url
+LOGIN_REDIRECT_URL = '/dashboard'
+LOGOUT_REDIRECT_URL = 'login'
