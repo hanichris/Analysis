08c43797a1767a47f71a2bd5149d197c82979b33
hanichris <hanichris71@gmail.com>
2024-06-05 19:45:33 +0300

    Change import ordering.
    update: using a class-based view for the index page.

--- backend/analysis/views.py
+++ backend/analysis/views.py
@@ -1,27 +1,33 @@
-from pathlib import Path
 import json
+import os
+from pathlib import Path
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.views.generic import CreateView, View
 
 from .forms import UserCreationForm
 from .models import User
 
-# Create your views here.
-def index(request: HttpRequest):
+
+class IndexView(LoginRequiredMixin, View):
     parents = Path(__file__).parents
     new_path = parents[1].joinpath('static/manifest.json')
     manifest: dict = json.load(new_path.open(encoding='utf-8'))
-    print(manifest)
-    return render(
-        request,
-        "analysis/dashboard.html",
-        {
-            'manifest': manifest.get('assets/main.tsx'),
-        },
-    )
+    access_token = os.getenv('MAPBOX_ACCESS_TOKEN')
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        return render(
+            request,
+            "analysis/dashboard.html",
+            {
+                "manifest": self.manifest,
+                "access_token": self.access_token,
+            }
+        )
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
