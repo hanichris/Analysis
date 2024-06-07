0e9f7b6ab2a20af703c653734611d9d96aada864
hanichris <hanichris71@gmail.com>
2024-06-08 00:01:14 +0300

    update: Configure layout for the routes.

--- backend/assets/main.tsx
+++ backend/assets/main.tsx
@@ -7,10 +7,28 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import ErrorPage from './error-page';
+import Root from './routes/root';
+
 const route = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello using React router</div>,
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "",
+                element: <div>Home page</div>,
+            },
+            {
+                path: "about",
+                element: <div>About page</div>,
+            },
+            {
+                path: "contact",
+                element: <div>Contact page</div>,
+            },
+        ],
     },
 ]);
 
