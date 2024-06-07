6cb5841840c80699ac2ae5a4a9cf317c99aded5b
hanichris <hanichris71@gmail.com>
2024-06-08 00:41:45 +0300

    Include Navbar and footer components in the root layout of the react-router application.

--- backend/assets/routes/root.tsx
+++ backend/assets/routes/root.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
 export default function Root() {
   return (
     <div className="container">
+        <Navbar />
       <div className="wrapper">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 }
