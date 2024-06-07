495dfbcc6ac88ed044341643c8e3aca31436c558
hanichris <hanichris71@gmail.com>
2024-06-08 00:41:15 +0300

    feat: page navbar component.

---
+++ backend/assets/components/navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to={""}>
+            <img src="/static/analysis/img/logo.svg" alt="logo" />
+          </Link>
+        </li>
+        <div>
+          <li>
+            <Link to={"about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"contact"}>Contact Us</Link>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
+}
