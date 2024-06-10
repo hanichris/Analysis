1aa187620c53cfcf845844f46c6e07669a268088
hanichris <hanichris71@gmail.com>
2024-06-11 02:51:33 +0300

    update Clean up about page.

--- backend/assets/routes/about.tsx
+++ backend/assets/routes/about.tsx
@@ -1,192 +1,194 @@
 export default function AboutPage() {
   return (
     <section>
-      <article>
-        <header>
-          <h2>
-            What we do now
-          </h2>
-        </header>
-        <p>
-          Divergent Space is a pioneering consultancy dedicated to harnessing
-          space technology's transformative potential for business growth and
-          positive societal change. Our mission revolves around seamlessly
-          integrating cutting-edge space solutions with visionary initiatives,
-          creating a unique synergy between profit-driven success and societal
-          impact. Specializing in strategic space consultancy, precise
-          implementation plans, and innovative projects, we are architects of a
-          transformative future. Our approach emphasizes divergent thinking,
-          unlocking creativity for space-driven business success, with a proven
-          track record of delivering tangible results in both profit and
-          societal transformation. Focused on impact, collaboration, and
-          excellence, Divergent Space invites individuals to engage by joining
-          STEM committees or supporting initiatives contributing to real-world
-          positive change. Our impact areas span space-powered business growth,
-          innovative projects addressing societal challenges, and excellence in
-          turning dreams into reality through space technology. Join us at
-          Divergent Space, where the convergence of space and societal
-          transformation shapes a future that is profitable and purposeful.
-        </p>
-        <div>
-          <img src="/static/analysis/img/satellite_side.jpg" alt="statellite-image" />
-        </div>
-      </article>
-
-      <article>
-        <header>
-          <h2>
-            Who we are
-          </h2>
-        </header>
-        <div>
-          <div>
-            <img src="/static/analysis/img/ceo.jpg" alt="image of CEO Mr. Albert Mbogo"/>
-          </div>
-          <div>
-            <h3>CEO</h3>
+      <div className="container">
+        <article>
+          <header>
             <h2>
-              Mr. Albert Mbogo
+              What we do now
             </h2>
-            <p>An entrepreneur, astrophysicist and our proud founder.</p>
+          </header>
+          <p>
+            Divergent Space is a pioneering consultancy dedicated to harnessing
+            space technology's transformative potential for business growth and
+            positive societal change. Our mission revolves around seamlessly
+            integrating cutting-edge space solutions with visionary initiatives,
+            creating a unique synergy between profit-driven success and societal
+            impact. Specializing in strategic space consultancy, precise
+            implementation plans, and innovative projects, we are architects of a
+            transformative future. Our approach emphasizes divergent thinking,
+            unlocking creativity for space-driven business success, with a proven
+            track record of delivering tangible results in both profit and
+            societal transformation. Focused on impact, collaboration, and
+            excellence, Divergent Space invites individuals to engage by joining
+            STEM committees or supporting initiatives contributing to real-world
+            positive change. Our impact areas span space-powered business growth,
+            innovative projects addressing societal challenges, and excellence in
+            turning dreams into reality through space technology. Join us at
+            Divergent Space, where the convergence of space and societal
+            transformation shapes a future that is profitable and purposeful.
+          </p>
+          <div>
+            <img src="/static/analysis/img/satellite_side.jpg" alt="statellite-image" />
           </div>
-        </div>
+        </article>
 
-        <div>
-          <div>
-            <img src="/static/analysis/img/coo.jpg" alt="image of COO Mr. Joshua Oleko"/>
-          </div>
-          <div>
-            <h3>
-              COO
-            </h3>
+        <article>
+          <header>
             <h2>
-              Mr. Joshua Oleko
+              Who we are
             </h2>
-            <p>
-              A Biosystems engineer who has had international experience in food
-              production and sustainable agriculture.
-            </p>
-          </div>
-        </div>
-
-        <div>
+          </header>
           <div>
-            <img src="/static/analysis/img/cto.jpeg" alt="image of CTO Ms. Evelyn"/>
-          </div>
-          <div>
-            <h3>
-              CTO
-            </h3>
-            <h2>
-              Ms. Evelyn
-            </h2>
-            <p>
-              A data scientist with a wealth of experience in statistics and
-              software engineering.
-            </p>
-          </div>
-        </div>
-      </article>
-
-
-      <article>
-        <header>
-          <h2>
-            Our Approach
-          </h2>
-        </header>
-        <div>
-          <ol>
-            <li>
-              Divergent Space Consultancy: Craft business strategies powered by
-              space technology.
-            </li>
-            <li>
-              Implementation Expertise: Seamlessly execute strategies for
-              tangible, real-world impact.
-            </li>
-            <li>
-              Innovative Initiatives: Turning dreams into reality through space
-              technology.
-            </li>
-          </ol>
-        </div>
-      </article>
-
-      <article>
-        <header>
-          <h2>
-            Why Choose Us
-          </h2>
-        </header>
-        <div>
-          <div>
-            <img src="/static/analysis/img/divergent.svg" alt=""/>
-            <h2>Divergent Thinking</h2>
-            <span>Unleashing creativity for space-driven business success</span>
-          </div>
-          <div>
-            <img src="/static/analysis/img/profit.svg" alt=""/>
-            <h2>Proven Impact</h2>
-            <span>
-              Tangible results in both profit and societal transformation
-            </span>
-          </div>
-        </div>
-
-        <div>
-          <div>
-            <h1>
-              Get Involved
-            </h1>
-            <h2>
-              Join a Committee
-            </h2>
-            <span>
-              Contribute to impactful projects through our STEM committees
-            </span>
-            <h2>
-              Support Our Projects
-            </h2>
-            <span>
-              Be part of the change and support initiatives that transform lives.
-            </span>
-          </div>
-
-          <div>
-            <header>
-              <h1>
-                Impact Areas
-              </h1>
-            </header>
-
             <div>
-              <span>
-                Space-Powered Business
-              </span>
-              : Competitive advantage and success for our clients.
-
-              <span>
-                Societal Development
-              </span>
-              : Innovative projects addressing real-world challenges.
-
-              <span>
-                Innovation Excellence
-              </span>
-              : Turning dreams into reality through space technology.
+              <img src="/static/analysis/img/ceo.jpg" alt="image of CEO Mr. Albert Mbogo"/>
+            </div>
+            <div>
+              <h3>CEO</h3>
+              <h2>
+                Mr. Albert Mbogo
+              </h2>
+              <p>An entrepreneur, astrophysicist and our proud founder.</p>
             </div>
           </div>
-        </div>
-    
-        <div>
-          <p>
-            Join us at Divergent Space Strategies, where space and societal
-            transformation converge for a future that's both profitable and
-            purposeful.
-          </p>
-        </div>
-      </article>
+
+          <div>
+            <div>
+              <img src="/static/analysis/img/coo.jpg" alt="image of COO Mr. Joshua Oleko"/>
+            </div>
+            <div>
+              <h3>
+                COO
+              </h3>
+              <h2>
+                Mr. Joshua Oleko
+              </h2>
+              <p>
+                A Biosystems engineer who has had international experience in food
+                production and sustainable agriculture.
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <img src="/static/analysis/img/cto.jpeg" alt="image of CTO Ms. Evelyn"/>
+            </div>
+            <div>
+              <h3>
+                CTO
+              </h3>
+              <h2>
+                Ms. Evelyn
+              </h2>
+              <p>
+                A data scientist with a wealth of experience in statistics and
+                software engineering.
+              </p>
+            </div>
+          </div>
+        </article>
+
+
+        <article>
+          <header>
+            <h2>
+              Our Approach
+            </h2>
+          </header>
+          <div>
+            <ol>
+              <li>
+                Divergent Space Consultancy: Craft business strategies powered by
+                space technology.
+              </li>
+              <li>
+                Implementation Expertise: Seamlessly execute strategies for
+                tangible, real-world impact.
+              </li>
+              <li>
+                Innovative Initiatives: Turning dreams into reality through space
+                technology.
+              </li>
+            </ol>
+          </div>
+        </article>
+
+        <article>
+          <header>
+            <h2>
+              Why Choose Us
+            </h2>
+          </header>
+          <div>
+            <div>
+              <img src="/static/analysis/img/divergent.svg" alt=""/>
+              <h2>Divergent Thinking</h2>
+              <span>Unleashing creativity for space-driven business success</span>
+            </div>
+            <div>
+              <img src="/static/analysis/img/profit.svg" alt=""/>
+              <h2>Proven Impact</h2>
+              <span>
+                Tangible results in both profit and societal transformation
+              </span>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <h1>
+                Get Involved
+              </h1>
+              <h2>
+                Join a Committee
+              </h2>
+              <span>
+                Contribute to impactful projects through our STEM committees
+              </span>
+              <h2>
+                Support Our Projects
+              </h2>
+              <span>
+                Be part of the change and support initiatives that transform lives.
+              </span>
+            </div>
+
+            <div>
+              <header>
+                <h1>
+                  Impact Areas
+                </h1>
+              </header>
+
+              <div>
+                <span>
+                  Space-Powered Business
+                </span>
+                : Competitive advantage and success for our clients.
+
+                <span>
+                  Societal Development
+                </span>
+                : Innovative projects addressing real-world challenges.
+
+                <span>
+                  Innovation Excellence
+                </span>
+                : Turning dreams into reality through space technology.
+              </div>
+            </div>
+          </div>
+      
+          <div>
+            <p>
+              Join us at Divergent Space Strategies, where space and societal
+              transformation converge for a future that's both profitable and
+              purposeful.
+            </p>
+          </div>
+        </article>
+      </div>
     </section>
   );
 }
