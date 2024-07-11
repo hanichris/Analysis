d2b6d3f8a10b3cc951de4ffca46dcd5835955e9f
hanichris <hanichris71@gmail.com>
2024-07-11 21:57:57 +0300

    Set up the contact page component.

--- frontend/src/routes/contact.tsx
+++ frontend/src/routes/contact.tsx
@@ -1,5 +1,17 @@
 export default function Contact() {
   return (
-    <h1>Contact Page</h1>
+    <section className="section">
+      <div className="container">
+        <div className="post-comments">
+          <header>
+            <h3 className="section-title">Get in touch</h3>
+            <p>Would you like to discuss your project and see how Divergent AG can help you? Fill the form and our team will contact you shortly.</p>
+          </header>
+          <form action="#" method="post">
+
+          </form>
+        </div>
+      </div>
+    </section>
   );
 }
