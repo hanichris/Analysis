1a8f62874975a33c49cdcd5ce06e30a22d566733
hanichris <hanichris71@gmail.com>
2024-07-11 23:46:49 +0300

    change vite's build configuration to integrate with the django backend.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import * as path from "path";
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    manifest: "manifest.json",
+    outDir: path.resolve('../backend/static'),
+    rollupOptions: {
+      input: {
+        key0001: "./src/main.tsx",
+      },
+      output: {
+        entryFileNames: "index-bundle.js",
+      }
+    }
+  }
 })
