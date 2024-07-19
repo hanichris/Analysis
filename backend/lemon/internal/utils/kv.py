df297a8079428f802118c74f8ac47d6e93f0e1d6
hanichris <hanichris71@gmail.com>
2024-07-20 01:59:51 +0300

    Add documentation.

--- backend/lemon/internal/utils/kv.py
+++ backend/lemon/internal/utils/kv.py
@@ -4,7 +4,21 @@ from typing import Any
 kv: dict[str, Any] = {}
 
 def get_kv(key: str) -> Any | None:
+    """Get the `value` corresponding to the `key`.
+
+    Args:
+        key: String type key.
+
+    Returns:
+        `Value` corresponding to the `key` passed in. `None`, otherwise.
+    """
     return kv.get(key)
 
 def set_kv(key: str, value: Any) -> None:
+    """Set the `value` corresponding to the `key`.
+
+    Args:
+        key: String type key.
+        value: The value to be set.
+    """
     kv[key] = value
