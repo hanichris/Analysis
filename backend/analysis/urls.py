2c37700facba833864ae40db2f2d95a4c7b2e6db
hanichris <hanichris71@gmail.com>
2024-07-26 02:02:37 +0300

    Update: add a url path to a user's profile page.

--- backend/analysis/urls.py
+++ backend/analysis/urls.py
@@ -6,6 +6,7 @@ app_name = "analysis"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("dashboard/", views.DashboardView.as_view(), name='dashboard'),
+    path("users/<uuid:pk>", views.UserProfileView.as_view(), name='profile'),
     path("accounts/signup/", views.SignUpView.as_view(), name='signup'),
     re_path("(about|contact)/", views.IndexView.as_view()),
 ]
