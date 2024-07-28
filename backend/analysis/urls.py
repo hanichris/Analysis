b1be5d2f2aefcf216b1be22350255d962f5b54f1
hanichris <hanichris71@gmail.com>
2024-07-28 23:56:25 +0300

    Cache the homepage

--- backend/analysis/urls.py
+++ backend/analysis/urls.py
@@ -1,10 +1,11 @@
+from django.views.decorators.cache import cache_page
 from django.urls import path, re_path
 
 from . import views
 
 app_name = "analysis"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", cache_page(60 * 60)(views.IndexView.as_view()), name="index"),
     path("dashboard/", views.DashboardView.as_view(), name='dashboard'),
     path("users/<uuid:pk>", views.UserProfileView.as_view(), name='profile'),
     path("accounts/signup/", views.SignUpView.as_view(), name='signup'),
