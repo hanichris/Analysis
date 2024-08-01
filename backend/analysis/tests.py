02dce772c35c11254ebe02bb15331d9199ca6644
hanichris <hanichris71@gmail.com>
2024-08-02 02:46:06 +0300

    Update test file.

--- backend/analysis/tests.py
+++ backend/analysis/tests.py
@@ -1,4 +1,7 @@
+from io import StringIO
+
 from django.contrib.auth import get_user_model
+from django.core.management import call_command
 from django.test import TestCase
 
 from .models import User as UserClass
@@ -13,7 +16,7 @@ class UserManagersTests(TestCase):
         `is_superuser=False` and `name=null`.
         """
         User = get_user_model()
-        user: UserClass = User.objects.create_user( # type: ignore
+        user: UserClass = User.people.create_user( # type: ignore
             email="normal@user.com",
             password="foo",
         )
@@ -24,13 +27,13 @@ class UserManagersTests(TestCase):
         self.assertIsNone(user.name)
 
         with self.assertRaises(TypeError):
-            User.objects.create_user() # type: ignore
+            User.people.create_user() # type: ignore
         with self.assertRaises(TypeError):
-            User.objects.create_user(email="") # type: ignore
+            User.people.create_user(email="") # type: ignore
         with self.assertRaises(ValueError):
-            User.objects.create_user(email="", password="foo") # type: ignore
+            User.people.create_user(email="", password="foo") # type: ignore
         with self.assertRaises(ValueError):
-            User.objects.create_user(email="test@test.com", password="") # type: ignore
+            User.people.create_user(email="test@test.com", password="") # type: ignore
     
     def test_create_superuser(self):
         """
@@ -39,7 +42,7 @@ class UserManagersTests(TestCase):
         `is_superuser=True` and `name=null`
         """
         User = get_user_model()
-        admin_user: UserClass = User.objects.create_superuser( # type: ignore
+        admin_user: UserClass = User.people.create_superuser( # type: ignore
             email="super@user.com",
             password="bar"
         )
@@ -50,16 +53,21 @@ class UserManagersTests(TestCase):
         self.assertIsNone(admin_user.name)
 
         with self.assertRaises(ValueError):
-            User.objects.create_superuser( # type: ignore
+            User.people.create_superuser( # type: ignore
                 email="super@user.com",
                 password="bar",
                 is_staff=False
             )
         with self.assertRaises(ValueError):
-            User.objects.create_superuser( # type: ignore
+            User.people.create_superuser( # type: ignore
                 email="super@user.com",
                 password="bar",
                 is_superuser=False
             )
         
 
+class SyncPlansTest(TestCase):
+    def test_command_operation(self):
+        out = StringIO()
+        call_command("syncPlans", stdout=out)
+        self.assertRaises(ValueError)
