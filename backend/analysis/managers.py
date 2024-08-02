c749c9a3deb47f185b55c96c7d38fc64a152897b
hanichris <hanichris71@gmail.com>
2024-08-03 00:03:38 +0300

    Add asynchronous versions of the creation methods.

--- backend/analysis/managers.py
+++ backend/analysis/managers.py
@@ -59,3 +59,60 @@ class CustomUserManager(BaseUserManager):
             password=password,
             **kwargs
         )
+    
+    async def acreate_user(self, /, *,  email: str, password: str, **kwargs):
+        """
+        Create and save a user with the given email and password.
+
+        The method is an asynchronous version of the `create_user` method
+        that has use in the `management` module.
+
+        Args:
+            email: str
+            password: str
+
+        Return:
+            user instance.
+
+        Raises:
+            ValueError
+        """
+        if not email:
+            raise ValueError(_("Users must have an email address"))
+
+        if not password:
+            raise ValueError(_("Users must have a password"))
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **kwargs
+        )
+        user.set_password(password)
+        await user.asave()
+        return user
+
+    async def acreate_superuser(self, email: str, password: str, **kwargs):
+        """
+        Create and save a superuser with the given email and password.
+
+        The method is an asynchronous version of the `create_superuser` method.
+        Args:
+            email: str
+            password: str
+
+        Return:
+            user instance
+        """
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault("is_superuser", True)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError(_("Superuser must have 'is_staff'=True"))
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError(_("Superuser must have 'is_superuser'=True"))
+
+        return await self.acreate_user(
+            email=email,
+            password=password,
+            **kwargs
+        )
