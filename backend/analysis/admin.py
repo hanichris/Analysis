15628f99a1d8848fec8e4e5e778da5b4e0d5ca25
hanichris <hanichris71@gmail.com>
2024-08-03 00:04:10 +0300

    Register the `Geofield` and `Comment` models with the admin.

--- backend/analysis/admin.py
+++ backend/analysis/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User
+from .models import User, Geofield, Comment
 from .forms import UserChangeForm, UserCreationForm
 
 # Register your models here.
@@ -33,3 +33,5 @@ class UserAdmin(BaseUserAdmin):
     ordering = ("email",)
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Geofield)
+admin.site.register(Comment)
