7ac4cde76e89d1978c34bdc070caa8e7befd7aef
hanichris <hanichris71@gmail.com>
2024-08-05 22:55:01 +0300

    transform parameters into a format acceptable to the lemon squeezy endpoint.

---
+++ backend/lemon/internal/utils/util.py
@@ -0,0 +1,16 @@
+import json
+from ...types.response import Params
+
+
+def params_to_query_string(params: Params) -> dict:
+    valid_params = params.model_dump()
+    valid_params['include'] = ','.join(valid_params['include'])
+    search_params = {}
+    for key in valid_params:
+        val = valid_params[key]
+        if isinstance(val, dict):
+            for entry in val:
+                search_params[f'{key}[{entry}]'] = f'{val[entry]}'
+        else:
+            search_params[key] = f'{val}' if val else val
+    return search_params
