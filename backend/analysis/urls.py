d3bed031fac30b8ba54949bde843be2c10f85174
hanichris <hanichris71@gmail.com>
2024-08-10 22:44:31 +0300

    add new `reports/upload` url

--- backend/analysis/urls.py
+++ backend/analysis/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", cache_page(60 * 60)(views.IndexView.as_view()), name="index"),
     path("dashboard/", views.DashboardView.as_view(), name='dashboard'),
     path("users/<uuid:pk>", views.UserProfileView.as_view(), name='profile'),
+    path("reports/upload", views.UploadReport.as_view(), name='report_upload'),
     path("accounts/signup/", views.SignUpView.as_view(), name='signup'),
     re_path("(about|contact)/", views.IndexView.as_view()),
 ]
