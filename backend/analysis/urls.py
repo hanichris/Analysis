109460a76be96711b7cba5d8b07cc17cc88fce88
hanichris <hanichris71@gmail.com>
2024-08-22 04:01:20 +0300

    do not cache administrative page.

--- backend/analysis/urls.py
+++ backend/analysis/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("dashboard/", views.DashboardView.as_view(), name='dashboard'),
     path("users/<uuid:pk>", views.UserProfileView.as_view(), name='profile'),
     path("download/<int:pk>", views.download_file, name="download_file"),
-    path("reports/upload", cache_page(60 * 60)(views.UploadReport.as_view()), name='report_upload'),
+    path("reports/upload", views.UploadReport.as_view(), name='report_upload'),
     path("accounts/signup/", views.SignUpView.as_view(), name='signup'),
     re_path("(about|contact)/", views.IndexView.as_view()),
 ]
