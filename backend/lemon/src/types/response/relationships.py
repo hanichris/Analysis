83fbd5048b2322c4b4c93b3b15c6813f6d0d6440
hanichris <hanichris71@gmail.com>
2024-08-23 22:33:00 +0300

    refactor Pick class. Moved to the relationship module.

--- backend/lemon/src/types/response/relationships.py
+++ backend/lemon/src/types/response/relationships.py
@@ -1,8 +1,10 @@
-from collections.abc import Iterator
-from typing import Literal, TypedDict
+from collections.abc import Iterator, Sequence
+from typing import Literal, Generic, TypedDict, TypeVar
 
-from pydantic import RootModel, BaseModel
+from pydantic import BaseModel, create_model, ConfigDict, RootModel
 
+
+T = TypeVar('T')
 
 type Types = Literal[
     "stores",
@@ -53,6 +55,9 @@ class RelationshipLinks(BaseModel, revalidate_instances='always'):
     link: Links
     data: list[Data] | None = None
 
+    def __getitem__(self, item) -> Links | list[Data] | None:
+        return getattr(self, item)
+
 
 class Relationship(RootModel):
     root: dict[RelationshipKeys, RelationshipLinks]
@@ -62,3 +67,55 @@ class Relationship(RootModel):
 
     def __iter__(self) -> Iterator[RelationshipKeys]:
         return iter(self.root)
+
+class Pick(BaseModel, Generic[T]):
+    keys: Sequence[T]
+
+    def pick(self):
+        config = {
+            'extra': 'forbid',
+            'validate_assignment': True
+        }
+        keys_model = create_model(
+            'Keys',
+            __config__=ConfigDict(**config),
+            **{key: (RelationshipLinks, ...) for key in self.keys}
+        ) # type: ignore
+        class Keys(RootModel):
+            root: keys_model # type: ignore
+
+            def __getitem__(self, item) -> RelationshipLinks:
+                return getattr(self.root, item)
+
+            def __iter__(self) -> Iterator[T]:
+                return iter(self.root)
+        
+        return Keys
+
+
+if __name__ == "__main__":
+
+    Model = Pick[RelationshipKeys](keys=['store', 'variant']).pick()
+    
+    data = {
+        'store': {
+            'link': {
+                'related': '',
+                'self': '',
+            },
+            'data': [
+                {
+                    'id': '',
+                    'type': 'discounts',
+                }
+            ]
+        },
+        'variant': {
+            'link': {
+                'related': '',
+                'self': '',
+            }
+        },
+    }
+    valid = Model(data)
+    
