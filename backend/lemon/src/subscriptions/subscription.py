ae29f4dbf5816227e1242c75a4e6ad2e2b9b032c
hanichris <hanichris71@gmail.com>
2024-08-26 23:37:49 +0300

    Update function docstrings.

--- backend/lemon/src/subscriptions/subscription.py
+++ backend/lemon/src/subscriptions/subscription.py
@@ -9,7 +9,24 @@ from .types import (
 )
 
 async def get_subscription(subscription_id: int | str, params: dict = {}):
-    """"""
+    """Retrieve a subscription.
+
+    Makes a `GET` request with an optional set of path parameters to the
+    lemonSqueezy API for the subscription with the given id.
+
+    Args:
+        `subscription_id`: The given subscription id.
+        `params`: (Optional) Additional parameters.
+        `params['include']`: (Optional) Related resources.
+    
+    Returns:
+        Response object with the keys `data`, `error` and `status_code`.
+        The `data` key holds the value of the subscription object requested.
+
+    Raises:
+        `ValidationError`: If the parameters or response do not match the expected
+        pydantic schemas.
+    """
     options = FetchOptions(
         path=f"/v1/subscriptions/{subscription_id}",
         param=include_to_query_string(
@@ -29,16 +46,16 @@ async def updated_subscription(
     provided id.
 
     Args:
-        subscription_id: The given subscription id.
-        update_subscription: Information that needs to be updated.
-    
+        `subscription_id`: The given subscription id.
+        `update_subscription`: Information that needs to be updated.
+
     Returns:
         Response object with the keys `data`, `error` and `status_code`. The
         `data` key holds the subscription object.
-    
-    Raises:
-        ValidationError: 
 
+    Raises:
+        `ValidationError`: If the parameters or response do not match the expected
+        pydantic schemas.
     """
     attributes = {
         'variant_id': update_subscription.get('variant_id'),
@@ -65,7 +82,19 @@ async def updated_subscription(
     return FetchResponse[Subscription](**await fetch(options)).model_dump()
 
 async def cancel_subscription(subscription_id: int | str):
-    """"""
+    """Cancel a subscription.
+
+    Args:
+        `subscription_id`: The given subscription id.
+    
+    Returns:
+        Response object with the keys `data`, `error` and `status_code`. The
+        `data` key holds the subscription object in a cancelled state.
+
+    Raises:
+        `ValidationError`: If the parameters or response do not match the expected
+        pydantic schemas.
+    """
     options = FetchOptions(
         path=f"/v1/subscriptions/{subscription_id}",
         method=HTTPVerbEnum.DELETE,
@@ -73,6 +102,43 @@ async def cancel_subscription(subscription_id: int | str):
     return FetchResponse[Subscription](**await fetch(options)).model_dump()
 
 async def list_subscriptions(params: dict = {}):
+    """Retrieve a list of subscriptions.
+
+    Makes a `GET` request with an optional set of path parameters to the
+    lemonSqueezy API for a list of subscriptions.
+
+    Args:
+        `params`: (Optional) Additional parameters.
+        `params['filter']`: (Optional) Filter parameters.
+        `params['filter']['store_id']`: (Optional) Only return subscriptions
+        belonging to the store with this ID.
+        `params['filter']['order_item_id']`: (Optional) Only return subscriptions
+        belonging to the order item with this ID.
+        `params['filter']['user_email']`: (Optional) Only return subscriptions
+        where the `user_email` field is equal to this email address.
+        `params['filter']['variant_id']`: (Optional) Only return subscriptions
+        belonging to the variant with this ID.
+        `params['filter']['order_id']`: (Optional) Only return subscriptions
+        belonging to the order with this ID.
+        `params['filter']['product_id']`: (Optional) Only return subscriptions
+        belonging to the product with this ID.
+        `params['page']`: (Optional) Custom paginated queries.
+        `params['page']['number']`: (Optional) The parameter determine which page
+        to retrieve.
+        `params['page']['size']`: (Optional) The parameter to determine how many
+        results to return per page.
+        `params['include']`: (Optional) Related resources.
+
+    Returns:
+        Response object with the keys `data`, `error`, and `status_code`. The
+        `data` key holds the paginated list of subscription objects ordered by
+        `created_at` (descending).
+
+    Raises:
+        ValidationError: If the parameters passed do not match the required
+        signature or if the LemonSqueezy API response doesn't match the provided
+        pydantic schema.
+    """
     options = FetchOptions(
         path='/v1/subscriptions',
         param=params_to_query_string(ListSubscriptionParams(**params))
