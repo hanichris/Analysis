43dc5a6ab602ec1bd9d9dcecedfb5ff8fc16afba
hanichris <hanichris71@gmail.com>
2024-08-29 02:04:41 +0300

    Create a new admin command to setup a webhook on lemon squeezy.

---
+++ backend/analysis/management/commands/setupWebhook.py
@@ -0,0 +1,14 @@
+import asyncio
+
+from typing import Any
+from django.core.management.base import BaseCommand
+
+from analysis.config import setup_webhook
+
+
+class Command(BaseCommand):
+    help = "Set up a webhook on Lemon Squeezy store."
+
+    def handle(self, *args: Any, **options: Any) -> str | None:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(setup_webhook()) 
