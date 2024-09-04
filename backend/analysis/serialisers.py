ab66207f001c32136d1ea79a4916a24c36c2388e
hanichris <hanichris71@gmail.com>
2024-09-05 02:37:01 +0300

    fix typo

--- backend/analysis/serialisers.py
+++ backend/analysis/serialisers.py
@@ -6,8 +6,8 @@ from analysis.models import WebhookEvent
 class WebhookEventSerializer(serializers.Serializer):
     body = serializers.JSONField()
     event_name = serializers.CharField()
-    proccessed = serializers.BooleanField(default=False)
-    proccessing_error = serializers.CharField(default='')
+    processed = serializers.BooleanField(default=False)
+    processing_error = serializers.CharField(default='')
 
     def create(self, validated_data):
         """Create and return a new `WebhookEvent` instance from the validated data.
@@ -16,10 +16,10 @@ class WebhookEventSerializer(serializers.Serializer):
     
     def update(self, instance, validated_data):
         instance.body = validated_data.get('body', instance.body)
-        instance.event_name = validated_data.get('body', instance.event_name)
-        instance.proccessed = validated_data.get('proccessed', instance.proccessed)
-        instance.proccessing_error = validated_data.get(
-            'proccessing_error', instance.proccessing_error
+        instance.event_name = validated_data.get('event_name', instance.event_name)
+        instance.processed = validated_data.get('processed', instance.processed)
+        instance.processing_error = validated_data.get(
+            'processing_error', instance.processing_error
         )
 
         instance.save()
