d725574fc4c61f98c3042c9f54d56e3075be525b
hanichris <hanichris71@gmail.com>
2024-09-06 18:43:37 +0300

    check that the webhook event received has the meta and data attributes in its structure.

--- backend/analysis/config/util.py
+++ backend/analysis/config/util.py
@@ -1,18 +1,32 @@
-from typing import NotRequired, TypedDict
+from typing import TypedDict
 
 from pydantic import BaseModel, ValidationError
 
+class CustomData(TypedDict):
+    user_id: str
+
 class Meta(TypedDict):
     event_name: str
-    custom_data: NotRequired[dict]
+    custom_data: CustomData
 
-class Event(BaseModel):
+class EventMetaAttribute(BaseModel):
     meta: Meta
+
+class EventDataAttribute(BaseModel):
+    data: dict
 
 def webhook_has_meta(obj):
     valid = True
     try:
-        Event.model_validate(obj)
+        EventMetaAttribute.model_validate(obj)
+    except ValidationError:
+        valid = False
+    return valid
+
+def webhook_has_data(obj):
+    valid = True
+    try:
+        EventDataAttribute.model_validate(obj)
     except ValidationError:
         valid = False
     return valid
