f540ca0d9286072da4382d35f7095f9fed08a29e
hanichris <hanichris71@gmail.com>
2024-09-08 02:22:49 +0300

    perform code refactor.

--- backend/analysis/views.py
+++ backend/analysis/views.py
@@ -10,9 +10,8 @@ from datetime import date
 from pathlib import Path
 from typing import cast
 
-from django.contrib.auth import aauthenticate, alogin, update_session_auth_hash, aupdate_session_auth_hash # type: ignore
+from django.contrib.auth import aauthenticate, alogin
 from django.core.paginator import Paginator
-from django.core.files.uploadedfile import UploadedFile
 from django.db import Error
 from django.http import HttpRequest, JsonResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render, redirect, aget_object_or_404 # type: ignore
@@ -23,11 +22,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
 
-from pydantic import BaseModel, EmailStr, ValidationError
+from pydantic import ValidationError
 
-from .serialisers import WebhookEventSerializer
-from .tasks import send_to_zoho, process_webhook_event
-from .config import sync_plans, get_checkout_url, webhook_has_meta
 from .forms import (
     UserCreationForm,
     UploadFileForm,
@@ -36,17 +32,14 @@ from .forms import (
     UserEditNameForm,
     UserEditPasswordForm,
 )
+from .config import get_checkout_url, webhook_has_meta
 from .mixins import AsyncLoginRequiredMixin, AsyncUserPassesTestMixin
 from .models import User, Comment, Report, Plan, WebhookEvent
+from .serialisers import WebhookEventSerializer
+from .tasks import send_to_zoho, process_webhook_event
+from .utils import PostData, get_user_reports, get_plans, save_report_files
 
 logger = logging.getLogger(__name__)
-
-class PostData(BaseModel):
-    first_name: str
-    last_name: str
-    email: EmailStr
-    title: str
-    comment: str
 
 @api_view(['POST'])
 def webhook(request: Request):
@@ -74,8 +67,6 @@ def webhook(request: Request):
 
     return Response('Invalid data', status=status.HTTP_400_BAD_REQUEST)
     
-
-
 class IndexView(View):
     parents = Path(__file__).parents
     new_path = parents[1].joinpath('static/manifest.json')
@@ -143,7 +134,7 @@ class SignUpView(View):
             password = form.cleaned_data['password1']
             user = await aauthenticate(request, email=email, password=password)
             await alogin(request, user)
-            # Send sing up email
+            # Send sign up email
             return redirect('/billing/')
         return render(
             request, 'registration/signup.html', {'form': form}
@@ -156,15 +147,10 @@ class SignUpView(View):
 #     success_url = reverse_lazy('login')
 #     template_name = "registration/signup.html"
 
-
 class UserProfileView(AsyncLoginRequiredMixin, View):
     async def get(self, request: HttpRequest, *args, **kwargs):
         user= cast(User, await request.auser()) # type: ignore
-        reports =  [
-            report
-            async for report in 
-            Report.objects.filter(user=user).order_by('-created_at')
-        ]
+        reports = await get_user_reports(user)
         page_number = request.GET.get('page')
         paginator = Paginator(reports, 3)
         page_obj = paginator.get_page(page_number)
@@ -288,7 +274,7 @@ class UploadReport(AsyncLoginRequiredMixin, AsyncUserPassesTestMixin, View):
         if form.is_valid():
             email = form.cleaned_data['email']
             user = await aget_object_or_404(User, email=email)
-            await self.handle_uploaded_file(user, form.cleaned_data['file'])
+            await save_report_files(user, form.cleaned_data['file'])
         else:
             return render(
                 request,
@@ -306,18 +292,6 @@ class UploadReport(AsyncLoginRequiredMixin, AsyncUserPassesTestMixin, View):
             }
         )
 
-    async def handle_uploaded_file(self, user: User, files: list[UploadedFile]):
-        uploaded_files = [
-            Report(
-                user=user,
-                file=file
-            )
-            for file in files
-        ]
-
-        await Report.objects.abulk_create(uploaded_files)
-
-
 async def download_file(request, pk: int):
     uploaded_file = await Report.objects.aget(pk=pk)
     print(uploaded_file.file.name)
@@ -325,10 +299,7 @@ async def download_file(request, pk: int):
 class Billing(AsyncLoginRequiredMixin, View):
     async def get(self, request: HttpRequest, *args, **kwargs):
         user = await request.auser() # type: ignore
-        count = await Plan.objects.acount()
-        plans = await sync_plans() if count == 0 else [
-            plan async for plan in Plan.objects.all()
-        ]
+        plans = await get_plans()
         if plans:
             async with asyncio.TaskGroup() as tg:
                 tasks = [
