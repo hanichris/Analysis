bbf358b11b4684784eae13a8a008fe97a292a5bc
hanichris <hanichris71@gmail.com>
2024-09-08 02:21:54 +0300

    refactor frequently used functions to enhance code modularity.

---
+++ backend/analysis/utils.py
@@ -0,0 +1,51 @@
+from django.core.files.uploadedfile import UploadedFile
+from pydantic import BaseModel, EmailStr
+
+from .config import cache_results, sync_plans
+from .models import Plan, Report, User
+
+class PostData(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    title: str
+    comment: str
+
+@cache_results(timeout=60)
+async def get_user_reports(user: User):
+    """Retrives the reports related to a particular user.
+
+    Args:
+        user: The user whose reports have been requested.
+    Returns:
+        list: All the reports of the particular individual.
+    """
+    return [
+        report
+        async for report in
+        Report.objects.filter(user=user).order_by('-created_at')
+    ]
+
+async def save_report_files(user: User, files: list[UploadedFile]):
+    """Saves the report files for a particular user in the database.
+
+    Args:
+        user: The user whose report files need to be saved in the database.
+        files: a list of report files uploaded to the server for a given user.
+    """
+    uploaded_files = [ Report(user=user, file=file) for file in files ]
+    await Report.objects.abulk_create(uploaded_files)
+
+async def get_plans():
+    """Retrieves the billing plans.
+
+    Fetches the plans from the database if they are available. Otherwise, retrieves
+    the plans from lemon squeezy and saves them in the database for future use. The
+    saved plans are then returned to the caller.
+    Returns:
+        A list of `Plan` model instances if available. Otherwise, `None`.
+    """
+    plans = [plan async for plan in Plan.objects.all()]
+    if len(plans) == 0:
+        plans = await sync_plans()
+    return plans
