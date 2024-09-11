c644efba8f5f0bca9f4e09d2eed25f5aaeca6529
hanichris <hanichris71@gmail.com>
2024-09-11 23:18:10 +0300

    updated import statement.

--- backend/analysis/config/__init__.py
+++ backend/analysis/config/__init__.py
@@ -1,3 +1,3 @@
 from .lemonsqueezy import configure_lemonsqueezy
-from .synchronisation import sync_plans, get_checkout_url, setup_webhook
+from .synchronisation import sync_plans, get_checkout_url, setup_webhook, get_user_subscriptions
 from .util import webhook_has_meta, webhook_has_data, cache_results
