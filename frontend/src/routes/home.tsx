58c0d3c4f9db156382fd3399c195ccfc6ec997a2
hanichris <hanichris71@gmail.com>
2024-09-16 23:32:19 +0300

    Add cta button

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -10,15 +10,43 @@ export default function Home() {
                   Boost Your Yield,{" "}
                   <span>
                     Dominate
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1418 125" fill="currentColor">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 1418 125"
+                      fill="currentColor"
+                    >
                       <path d="M1412.29 72.17c-11.04-5.78-20.07-14.33-85.46-25.24-22.37-3.63-44.69-7.56-67.07-11.04-167.11-22.06-181.65-21.24-304.94-30.56C888.78 1.39 822.57 1.1 756.44 0c-46.63-.11-93.27 1.56-139.89 2.5C365.5 13.55 452.86 7.68 277.94 23.15 202.57 33.32 127.38 45.01 52.07 55.69c-11.23 2.41-22.63 4.17-33.71 7.22C6.1 66.33 5.64 66.19 3.89 67.79c-7.99 5.78-2.98 20.14 8.72 17.5 33.99-9.47 32.28-8.57 178.06-29.66 4.26 4.48 7.29 3.38 18.42 3.11 13.19-.32 26.38-.53 39.56-1.12 53.51-3.81 106.88-9.62 160.36-13.95 18.41-1.3 36.8-3.12 55.21-4.7 23.21-1.16 46.43-2.29 69.65-3.4 120.28-2.16 85.46-3.13 234.65-1.52 23.42.99 1.57-.18 125.72 6.9 96.61 8.88 200.92 27.94 295.42 46.12 40.87 7.91 116.67 23.2 156.31 36.78 3.81 1.05 8.28-.27 10.51-3.58 3.17-3.72 2.66-9.7-.78-13.13-3.25-3.12-8.14-3.44-12.18-5.08-17.89-5.85-44.19-12.09-63.67-16.56l26.16 3.28c23.02 3.13 46.28 3.92 69.34 6.75 10.8.96 25.43 1.81 34.34-4.39 2.26-1.54 4.86-2.75 6.21-5.27 2.76-4.59 1.13-11.06-3.59-13.68ZM925.4 23.77c37.64 1.4 153.99 10.85 196.64 14.94 45.95 5.51 91.89 11.03 137.76 17.19 24.25 4.77 74.13 11.21 101.72 18.14-11.87-1.15-23.77-1.97-35.65-3.06-133.46-15.9-266.8-33.02-400.47-47.21Z"></path>
                     </svg>
-                  </span> 
-                  {" "}the Field.
+                  </span>{" "}
+                  the Field.
                 </h1>
                 <p className="subheading">
-                  With Divergent AG, you can get real-time analytics tailored to your farm's specific location to help in maximising your yield.
+                  With Divergent AG, you can get real-time analytics tailored to
+                  your farm's specific location to help in maximising your
+                  yield.
                 </p>
+                <div className="button-wrapper">
+                  <a href="/accounts/signup/" className="button-primary">
+                    <div className="button-primary--overlay">
+                      <div>Get started for free</div>
+                      <div className="button-primary--icon">
+                        <svg
+                          width="12"
+                          height="12"
+                          viewBox="0 0 12 12"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M6.39205 11.2102L5.36932 10.1988L8.92045 6.64768H0V5.17041H8.92045L5.36932 1.62496L6.39205 0.60791L11.6932 5.90905L6.39205 11.2102Z"
+                            fill="currentColor"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                  </a>
+                  <link rel="prefetch" href="/accounts/signup/" />
+                </div>
               </div>
               <div className="card-hero">
                 <div className="col-1-3">
@@ -32,7 +60,8 @@ export default function Home() {
                         static/analysis/img/millet/millet_c_scale,w_1358.jpg 1358w,
                         static/analysis/img/millet/millet_c_scale,w_2040.jpg 2040w,
                         static/analysis/img/millet/millet_c_scale,w_2382.jpg 2382w,
-                        static/analysis/img/millet/millet_c_scale,w_2600.jpg 2600w" />
+                        static/analysis/img/millet/millet_c_scale,w_2600.jpg 2600w"
+                    />
                   </div>
                 </div>
                 <div className="col-1-3">
@@ -46,7 +75,8 @@ export default function Home() {
                         static/analysis/img/maize/maize_c_scale,w_2405.jpg 2405w,
                         static/analysis/img/maize/maize_c_scale,w_2600.jpg 2600w"
                       src="static/analysis/img/maize/maize_c_scale,w_2600.jpg"
-                      alt="maize image" />
+                      alt="maize image"
+                    />
                   </div>
                 </div>
                 <div className="col-1-3">
@@ -60,14 +90,39 @@ export default function Home() {
                         static/analysis/img/wheet/wheet_c_scale,w_2245.jpg 2245w,
                         static/analysis/img/wheet/wheet_c_scale,w_2600.jpg 2600w"
                       src="static/analysis/img/wheet_c_scale,w_2600.jpg"
-                      alt="wheet image" />
+                      alt="wheet image"
+                    />
                   </div>
                 </div>
               </div>
               <div className="mobile-subheading">
                 <p className="subheading">
-                  With Divergent AG, you can get real-time analytics tailored to your farm's specific location to help in maximising your yield.
+                  With Divergent AG, you can get real-time analytics tailored to
+                  your farm's specific location to help in maximising your
+                  yield.
                 </p>
+                <div className="mobile-button-wrapper">
+                  <a href="/accounts/signup/" className="button-primary">
+                    <div className="button-primary--overlay">
+                      <div>Get started for free</div>
+                      <div className="button-primary--icon">
+                        <svg
+                          width="12"
+                          height="12"
+                          viewBox="0 0 12 12"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M6.39205 11.2102L5.36932 10.1988L8.92045 6.64768H0V5.17041H8.92045L5.36932 1.62496L6.39205 0.60791L11.6932 5.90905L6.39205 11.2102Z"
+                            fill="currentColor"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                  </a>
+                  <link rel="prefetch" href="/accounts/signup/" />
+                </div>
               </div>
             </section>
           </div>
@@ -85,21 +140,20 @@ export default function Home() {
                 <article className="card-services">
                   <div className="card-outer">
                     <div className="card-services-image">
-                    <img
-                      sizes="(max-width: 6500px) 40vw, 2600px"
-                      srcSet="
+                      <img
+                        sizes="(max-width: 6500px) 40vw, 2600px"
+                        srcSet="
                       static/analysis/img/aerial_farm/aerial_farm_c_scale,w_1872.jpg 1872w,
                       static/analysis/img/aerial_farm/aerial_farm_c_scale,w_1237.jpg 1237w,
                       static/analysis/img/aerial_farm/aerial_farm_c_scale,w_2328.jpg 2328w,
                       static/analysis/img/aerial_farm/aerial_farm_c_scale,w_2600.jpg 2600w"
-                      src="static/analysis/img/aerial_farm/aerial_farm_c_scale,w_2600.jpg"
-                      alt="Service 1" />
+                        src="static/analysis/img/aerial_farm/aerial_farm_c_scale,w_2600.jpg"
+                        alt="Service 1"
+                      />
                     </div>
                     <div className="card-inner">
                       <h3 className="card-title">
-                        <span>
-                          Service 1
-                        </span>
+                        <span>Service 1</span>
                       </h3>
                     </div>
                   </div>
@@ -118,13 +172,12 @@ export default function Home() {
                           static/analysis/img/satellite_side/satellite_side_c_scale,w_2134.jpg 2134w,
                           static/analysis/img/satellite_side/satellite_side_c_scale,w_2600.jpg 2600w"
                         src="static/analysis/img/satellite_side/satellite_side_c_scale,w_2600.jpg"
-                        alt="" />
+                        alt=""
+                      />
                     </div>
                     <div className="card-inner">
                       <h3 className="card-title">
-                        <span>
-                          Service 2
-                        </span>
+                        <span>Service 2</span>
                       </h3>
                     </div>
                   </div>
@@ -134,21 +187,20 @@ export default function Home() {
                 <article className="card-services">
                   <div className="card-outer">
                     <div className="card-services-image">
-                    <img
-                      sizes="(max-width: 6500px) 40vw, 2600px"
-                      srcSet="
+                      <img
+                        sizes="(max-width: 6500px) 40vw, 2600px"
+                        srcSet="
                       static/analysis/img/aerial_farm/aerial_farm_c_scale,w_1237.jpg 1237w,
                       static/analysis/img/aerial_farm/aerial_farm_c_scale,w_1872.jpg 1872w,
                       static/analysis/img/aerial_farm/aerial_farm_c_scale,w_2328.jpg 2328w,
                       static/analysis/img/aerial_farm/aerial_farm_c_scale,w_2600.jpg 2600w"
-                      src="static/analysis/img/aerial_farm/aerial_farm_c_scale,w_2600.jpg"
-                      alt="Service 3" />
+                        src="static/analysis/img/aerial_farm/aerial_farm_c_scale,w_2600.jpg"
+                        alt="Service 3"
+                      />
                     </div>
                     <div className="card-inner">
                       <h3 className="card-title">
-                        <span>
-                          Service 3
-                        </span>
+                        <span>Service 3</span>
                       </h3>
                     </div>
                   </div>
