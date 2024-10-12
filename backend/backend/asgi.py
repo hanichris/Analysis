9feaf476de58b9bf4eda3fa80433b80f4e93e090
hanichris <hanichris71@gmail.com>
2024-10-12 19:45:02 +0300

    remove channels lib

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -9,18 +9,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from analysis.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-    )
-})
+application = get_asgi_application()
